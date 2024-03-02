16f8159536576e59461ef47ab277a903ff22bd54
Minh <tranminhbvhp@gmail.com>
2024-03-02 20:32:42 +0700

    part 25

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Setup project dependencies:
 - npm install --save-dev @babel/core@7.24.0 @babel/node@7.23.9 @babel/preset-env@7.12.10 nodemon@2.0.22
 
 - npm install --save mysql2@2.2.5
+
+- npm install --save bcryptjs@2.4.3
--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,5 @@
 import db from '../models/index';
+import CRUDService from '../services/CRUDService';
 
 let getHomePage = async (req, res) => {
     try {
@@ -7,15 +8,27 @@ let getHomePage = async (req, res) => {
             data: JSON.stringify(data)
         });
     } catch (e) {
-        console.log(e)
+        console.log(e);
     }
 }
 
 let getAboutPage = (req, res) => {
-    return res.render('test/about.ejs')
+    return res.render('test/about.ejs');
+}
+
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs');
+}
+
+let postCRUD = async (req, res) => {
+    let message = await CRUDService.createNewUser(req.body);
+    console.log(message);
+    return res.send('post crud from server');
 }
 
 module.exports = {
     getHomePage: getHomePage,
-    getAboutPage: getAboutPage
+    getAboutPage: getAboutPage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
 }
