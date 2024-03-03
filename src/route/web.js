9eff20a42ec0668a806bce58b2ac9aaadea995d6
Minh <tranminhbvhp@gmail.com>
2024-03-03 11:17:14 +0700

    part 27

--- src/route/web.js
+++ src/route/web.js
@@ -6,11 +6,13 @@ let router = express.Router();
 let initWebRoutes = (app) => {
     router.get('/', homeController.getHomePage);
     router.get('/about', homeController.getAboutPage);
+
     router.get('/crud', homeController.getCRUD);
+    router.post('/post-crud', homeController.postCRUD);
     router.get('/get-crud', homeController.displayGetCRUD);
 
-    router.post('/post-crud', homeController.postCRUD);
-
+    router.get('/edit-crud', homeController.getEditCRUD);
+    router.post('/put-crud', homeController.putCRUD);
 
     return app.use("/", router);
 }
