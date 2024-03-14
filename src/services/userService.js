54ce5e6b768a861f682963a95389ea20c223d9fb
Minh <tranminhbvhp@gmail.com>
2024-03-14 14:11:12 +0700

    part 41

--- src/services/userService.js
+++ src/services/userService.js
@@ -112,26 +112,26 @@ let createNewUser = (data) => {
             if (check) {
                 resolve({
                     errCode: 1,
-                    message: 'Your email is already in used. Please try another email'
+                    errMessage: 'Your email is already in used. Please try another email'
+                })
+            } else {
+                let hashPasswordFromBcrypt = await hashUserPassword(data.password);
+                await db.User.create({
+                    email: data.email,
+                    password: hashPasswordFromBcrypt,
+                    firstName: data.firstName,
+                    lastName: data.lastName,
+                    address: data.address,
+                    phonenumber: data.phonenumber,
+                    gender: data.gender === '1' ? true : false,
+                    roleId: data.roleId,
+                });
+
+                resolve({
+                    errCode: 0,
+                    message: 'Ok'
                 })
             }
-
-            let hashPasswordFromBcrypt = await hashUserPassword(data.password);
-            await db.User.create({
-                email: data.email,
-                password: hashPasswordFromBcrypt,
-                firstName: data.firstName,
-                lastName: data.lastName,
-                address: data.address,
-                phonenumber: data.phonenumber,
-                gender: data.gender === '1' ? true : false,
-                roleId: data.roleId,
-            });
-
-            resolve({
-                errCode: 0,
-                message: 'Ok'
-            })
         } catch (e) {
             reject(e)
         }
