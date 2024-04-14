ef20034aeb64dbc0ab9c67738e7218dcaf60eec5
Minh <tranminhbvhp@gmail.com>
2024-04-14 12:57:49 +0700

    part 69

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -89,7 +89,7 @@ const getDetailDoctorById = (inputId) => {
                 let data = await db.User.findOne({
                     where: { id: inputId },
                     attributes: {
-                        exclude: ['password', 'image']
+                        exclude: ['password']
                     },
                     include: [
                         {
@@ -99,9 +99,17 @@ const getDetailDoctorById = (inputId) => {
 
                         { model: db.Allcode, as: 'positionData', attributes: ['valueEn', 'valueVi'] },
                     ],
-                    raw: true,
+                    raw: false,
                     nest: true
                 })
+
+                if (data && data.image) {
+                    data.image = new Buffer(data.image, 'base64').toString('binary');
+                }
+
+                if (!data) {
+                    data = {};
+                }
 
                 resolve({
                     errCode: 0,
